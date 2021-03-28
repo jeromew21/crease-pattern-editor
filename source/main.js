f065decaba4b8060f570bf03f96a35dedce7d89d
Jerome Wei <jeromew@berkeley.edu>
2021-03-28 05:26:33 -0700

    added circles

--- source/main.js
+++ source/main.js
@@ -1,4 +1,17 @@
-function getMousePos(canvas, evt) {
+const dragRatio = 1;
+
+function inBounds(coords, boundingRect) {
+    var x = coords.x;
+    var y = coords.y;
+    return !(x < boundingRect.x || x > boundingRect.x + boundingRect.w
+        || y < boundingRect.y || y > boundingRect.y + boundingRect.h);
+}
+
+function dist(c1, c2) {
+    var dx = c1.x - c2.x;
+    var dy = c1.y - c2.y;
+
+    return Math.sqrt(dx * dx + dy * dy);
 }
 
 class Square {
@@ -8,11 +21,95 @@ class Square {
         this.y = y; // absolute y
     }
 
+    name() {
+        return "square";
+    }
+
+    update() {
+
+    }
+
+    // return a rectangle of bounds relative to the object
+    rectBounds(offset, zoom) {
+        return {
+            x: this.x + offset.x,
+            y: this.y + offset.y,
+            w: this.size * zoom,
+            h: this.size * zoom
+        }
+    }
+
+    isPointInside(coords, offset, zoom) {
+        return inBounds(coords, this.rectBounds(offset, zoom));
+    }
+
     draw(ctx, offset, zoom) {
         const w = this.size * zoom;
         ctx.fillStyle = '#ffffff';
+        ctx.strokeStyle = '#000000'
+        ctx.lineWidth = 1;
         ctx.beginPath();
         ctx.fillRect(this.x + offset.x, this.y + offset.y, w, w);
+        ctx.stroke();
+        ctx.strokeRect(this.x + offset.x, this.y + offset.y, w, w);
+    }
+}
+
+class Circle {
+    static circleCount = 0;
+    static pink = "#FF0000";
+    static black = "#000000"
+
+    constructor(x, y, r, parent) {
+        this.x = x;
+        this.y = y;
+        this.radius = r;
+        this.parent = parent;
+        this.name_ = "circle " + Circle.circleCount;
+        this.color = Circle.black;
+        Circle.circleCount += 1;
+    }
+
+    name() {
+        return this.name_;
+    }
+
+    get coords() {
+        return { x: this.x, y: this.y };
+    }
+
+    update() {
+        for (var i = 0; i < this.parent.objects.length; i++) {
+            var obj = this.parent.objects[i];
+            if (obj instanceof Circle && obj != this) {
+                if (dist(this.coords, obj.coords) < this.radius + obj.radius) {
+                    this.color = Circle.pink;
+                    return;
+                }
+            }
+        }
+        this.color = Circle.black;
+    }
+
+    isPointInside(coords, offset, zoom) {
+        var dx = coords.x - (this.x + offset.x);
+        var dy = coords.y - (this.y + offset.y);
+
+        return Math.sqrt(dx * dx + dy * dy) < this.radius * zoom;
+    }
+
+    draw(ctx, offset, zoom) {
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = this.color;
+        ctx.fillStyle = this.color;
+
+        var r = this.radius * zoom;
+        ctx.beginPath();
+        ctx.arc(this.x + offset.x, this.y + offset.y, r, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(this.x + offset.x, this.y + offset.y, r/20, 0, 2 * Math.PI);
+        ctx.fill();
         ctx.stroke();
     }
 }
@@ -20,11 +117,15 @@ class Square {
 class Canvas {
     constructor(canvas) {
         this.canvas = canvas;
-        this.canvas.height = 500;
-        this.canvas.width = 500;
+        this.canvas.height = 800;
+        this.canvas.width = 900;
         this.context = canvas.getContext('2d');
+        this.context.strokeStyle = "#000000"
 
-        this.square = new Square(400, 0, 0);
+        // this.context.mozImageSmoothingEnabled = false;
+        this.context.ImageSmoothingEnabled = false;
+
+        this.square = new Square(500, 0, 0);
 
         this.mouse = {
             x: 0,
@@ -32,82 +133,294 @@ class Canvas {
         };
 
         this.offset = {
-            x: 0,
-            y: 0
+            x: 50,
+            y: 50
         };
 
         this.globalDrag = {
             x0: 0,
             y0: 0,
-            dx: 0,
-            dy: 0,
             offsetX0: 0,
             offsetY0: 0,
             dragging: false
         };
 
-        this.objects = [];
+        this.objectDrag = {
+            x0: 0,
+            y0: 0,
+            objX0: 0,
+            objY0: 0,
+            dragging: false
+        }
+
+        this.objects = [this.square];
 
         this.zoom = 1;
-        
+
+        this.grid = {
+            enabled: false,
+            count: 8,
+        }
+
+        this.diagonals = true;
+
         this.ui = {
             x: $("#ui-x"),
             y: $("#ui-y"),
             xOffset: $("#ui-x-offset"),
             yOffset: $("#ui-y-offset"),
-            inspector: $("#ui-inspector")
-        }
-        
+            grid: {
+                checkbox: $("#ui-grid"),
+                count: $("#ui-grid-count")
+            },
+            diagonals: $("#ui-diagonals"),
+            recenter: $("#ui-recenter"),
+            circle: {
+                window: $("#ui-circle-inspector"),
+                new: $("#ui-new-circle"),
+                name: $("#ui-circle-name"),
+                x: $("#ui-circle-x"),
+                y: $("#ui-circle-y"),
+                radius: $("#ui-circle-radius")
+            },
+            line: {
+                window: $("#ui-line-inspector")
+            },
+            angle: {
+                window: $("#ui-angle-inspector")
+            }
+        };
+
+        this.selectedObject = null;
+
+        this.ui.circle.window.hide();
+        this.ui.angle.window.hide();
+        this.ui.line.window.hide();
+
+        $("html").css("cursor", "default");
+
         self = this; // to make sure 'this' isn't stolen by scope...?
 
-        this.canvas.addEventListener("mousemove", this.updateMousePos);
+        this.canvas.addEventListener("mousemove", this.handleMove);
         this.canvas.addEventListener("mousedown", this.handleDown);
         this.canvas.addEventListener("mouseup", this.handleUp);
+        this.canvas.addEventListener("mouseleave", this.handleLeave);
+
+        this.ui.circle.new.click(function () {
+            self.createCircle();
+        });
+
+        this.ui.recenter.click(function () {
+            self.offset.x = 0;
+            self.offset.y = 0;
+        });
+
+        this.ui.grid.checkbox.prop('checked', self.grid.enabled);
+        if (!this.grid.enabled) {
+            this.ui.grid.count.prop('disabled', 'disabled');
+        }
+        this.ui.grid.count.val(self.grid.count);
+        this.ui.diagonals.val(self.diagonals);
+
+        this.ui.grid.checkbox.change(function () {
+            if ($(this).is(':checked')) {
+                self.grid.enabled = true;
+                self.ui.grid.count.prop('disabled', false);
+            } else {
+                self.grid.enabled = false;
+                self.ui.grid.count.prop('disabled', 'disabled');
+            }
+        });
+
+        this.ui.diagonals.change(function () {
+            if ($(this).is(':checked')) {
+                self.diagonals = true;
+            } else {
+                self.diagonals = false;
+            }
+        });
+
+        this.ui.grid.count.change(function () {
+            self.grid.count = $(this).val();
+        });
+    }
+
+    hideWindows() {
+        this.ui.circle.window.hide();
+        this.ui.angle.window.hide();
+        this.ui.line.window.hide();
+    }
+
+    drawGrid() {
+        // need to overhaul for generalized shapes...
+        var squareSize = this.square.size;
+        var stepSize = this.square.size / self.grid.count;
+        var overflowAmt = 500;
+        this.context.lineWidth = 0.5;
+        this.context.beginPath();
+        for (var i = -stepSize * 10; i < this.square.size + stepSize * 10; i += stepSize) {
+            //this.context.beginPath();
+            this.context.moveTo(-overflowAmt + this.offset.x, i + this.offset.y);
+            this.context.lineTo(squareSize + this.offset.x + overflowAmt, i + this.offset.y);
+            //this.context.stroke();
+
+            //this.context.beginPath();
+            this.context.moveTo(i + this.offset.x, -overflowAmt + this.offset.y);
+            this.context.lineTo(i + this.offset.x, squareSize + this.offset.y + overflowAmt);
+            //this.context.stroke();
+        }
+        this.context.stroke();
+    }
+
+    drawDiagonals() {
+        var squareSize = this.square.size;
+        this.context.lineWidth = 0.5;
+        this.context.strokeStyle = "#000000";
+        this.context.beginPath();
+        this.context.moveTo(this.offset.x, this.offset.y);
+        this.context.lineTo(squareSize + this.offset.x, squareSize + this.offset.y);
+        this.context.moveTo(this.offset.x + squareSize, this.offset.y);
+        this.context.lineTo(this.offset.x, squareSize + this.offset.y);
+        this.context.stroke();
+    }
+
+    updateObjects() {
+        for (var i = 0; i < self.objects.length; i++) {
+            var obj = self.objects[i];
+            obj.update();
+        }
     }
 
     update() {
-        this.context.clearRect(0, 0, canvas.width, canvas.height);
-        self.ui.xOffset.html(self.offset.x);
-        self.ui.yOffset.html(self.offset.y);
-        this.square.draw(canvas.context, this.offset, this.zoom);
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ui.xOffset.html(this.offset.x);
+        this.ui.yOffset.html(this.offset.y);
+        for (var i = 0; i < this.objects.length; i++) {
+            var obj = this.objects[i];
+            obj.draw(this.context, this.offset, this.zoom);
+        }
+        if (this.grid.enabled) {
+            this.drawGrid();
+        }
+        if (this.diagonals) {
+            this.drawDiagonals();
+        }
+    }
+
+    createCircle() {
+        var hw = this.square.size / 2;
+        var r1 = (Math.random() - 0.5) * hw;
+        var r2 = (Math.random() - 0.5) * hw;
+        var c = new Circle(this.square.x + hw + r1, this.square.y + hw + r2, this.square.size / 4, this);
+        this.objects.push(c);
+        this.updateObjects();
+    }
+
+    globalDragStop() {
+        if (self.globalDrag.dragging) {
+            console.log("drag end");
+            self.globalDrag.dragging = false;
+            $("html").css("cursor", "default");
+        } else if (self.objectDrag.dragging) {
+            self.objectDrag.dragging = false;
+        }
+    }
+
+    handleLeave(e) {
+        self.globalDragStop();
+    }
+
+    syncInspector() {
+        var obj = this.selectedObject;
+        if (obj instanceof Circle) {
+            this.ui.circle.name.val(obj.name());
+            this.ui.circle.x.val(obj.x);
+            this.ui.circle.y.val(obj.y);
+            this.ui.circle.radius.val(obj.radius);
+        }
+    }
+
+    selectObject(obj) {
+        if (obj instanceof Circle) {
+            this.hideWindows();
+            this.ui.circle.window.show();
+            this.selectedObject = obj;
+
+            this.syncInspector();
+        }
     }
 
     handleDown(e) {
-        if (self.globalDrag.dragging) {
-            // is dragging...?
-            var rect = self.canvas.getBoundingClientRect();
-            currX = e.clientX - rect.left;
-            currY = e.clientY - rect.top;
-            self.globalDrag.dx = self.globalDrag.x0 - currX;
-            self.globalDrag.dy = self.globalDrag.y0 - currY;
-
-            self.offset.x = self.globalDrag.offsetX0 += self.globalDrag.dx;
-            self.offset.y = self.globalDrag.offsetY0 += self.globalDrag.dx;
-        } else {
+        var obj = self.getObjectUnderneath(self.mouse);
+        if (obj != null) {
+            if (!(obj instanceof Square)) {
+                self.selectObject(obj);
+                console.log("obj drag start")
+                self.objectDrag.dragging = true;
+                self.objectDrag.x0 = self.mouse.x;
+                self.objectDrag.y0 = self.mouse.y;
+                self.objectDrag.objX0 = obj.x;
+                self.objectDrag.objY0 = obj.y;
+            }
+        } else if (!self.globalDrag.dragging) {
             console.log("drag start");
             // init global drag
             self.globalDrag.dragging = true;
             self.globalDrag.x0 = self.mouse.x;
             self.globalDrag.y0 = self.mouse.y;
+            self.globalDrag.offsetX0 = self.offset.x;
+            self.globalDrag.offsetY0 = self.offset.y;
+            $("html").css("cursor", "move");
         }
     }
 
     handleUp(e) {
-        if (self.globalDrag.dragging) {
-            console.log("drag end");
-            console.log(self.globalDrag);
-            self.globalDrag.dragging = false;
-            self.offset.x += self.globalDrag.dx;
-            self.offset.y += self.globalDrag.dy;
-        }
+        self.globalDragStop();
     }
 
-    updateMousePos(e) {
+    // given relative (ui) coords (x, y), return the object underneath.
+    getObjectUnderneath(coords) {
+        for (var i = this.objects.length - 1; i >= 0; i--) {
+            var obj = this.objects[i];
+            if (obj.isPointInside(coords, this.offset, this.zoom)) {
+                return obj;
+            }
+        }
+        return null;
+    }
+
+    handleMove(e) {
         var rect = self.canvas.getBoundingClientRect();
         self.mouse.x = e.clientX - rect.left;
         self.mouse.y = e.clientY - rect.top;
         self.ui.x.html(self.mouse.x);
         self.ui.y.html(self.mouse.y);
+
+        if (self.objectDrag.dragging) {
+            var dx = dragRatio * (self.objectDrag.x0 - self.mouse.x);
+            var dy = dragRatio * (self.objectDrag.y0 - self.mouse.y);
+
+            self.selectedObject.x = self.objectDrag.objX0 - dx;
+            self.selectedObject.y = self.objectDrag.objY0 - dy;
+
+            self.updateObjects();
+
+            self.syncInspector();
+        } if (self.globalDrag.dragging) {
+            // is dragging...?
+            var dx = dragRatio * (self.globalDrag.x0 - self.mouse.x);
+            var dy = dragRatio * (self.globalDrag.y0 - self.mouse.y);
+
+            self.offset.x = self.globalDrag.offsetX0 - dx;
+            self.offset.y = self.globalDrag.offsetY0 - dy;
+        } else {
+            // set cursor depending on object underneath
+            if (self.getObjectUnderneath(self.mouse) == null) {
+                $("html").css("cursor", "pointer");
+            } else {
+                $("html").css("cursor", "default");
+            }
+        }
     }
 }
 
