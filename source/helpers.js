3b303a47a152ea3cdb9a01e218e59a8d7a21924f
Jerome Wei <jeromew@berkeley.edu>
2021-05-17 16:05:30 -0700

    refactoring DOM stuff

--- source/helpers.js
+++ source/helpers.js
@@ -1,106 +1,115 @@
-const dragRatio = 1;
-const moveVelocity = 1;
-const rotationVelocity = .2;
-
-var keyRegister = {
-    w: false,
-    a: false,
-    s: false,
-    d: false,
-    q: false,
-    e: false,
-    Delete: false
-};
-
-document.onkeydown = function (e) {
-    keyRegister[e.key] = true;
+function nDigitRand(n) {
+    return Math.floor(Math.random() * Math.pow(10, n));
 }
 
-document.onkeyup = function (e) {
-    keyRegister[e.key] = false;
-}
-
-function degToRad(theta) {
-    return theta * (Math.PI / 180);
-}
-
-function rotate(coords, origin, theta) {
-    var sCoords = {
-        x: coords.x - origin.x,
-        y: coords.y - origin.y
-    };
-
-    var rad = degToRad(theta);
-
-    var rotated = {
-        x: sCoords.x*Math.cos(rad) - sCoords.y*Math.sin(rad),
-        y: sCoords.x*Math.sin(rad) + sCoords.y*Math.cos(rad)
+class Checkbox {
+    static html(domId, value, label, classes) {
+        var checked = " ";
+        if (value) {
+            checked = " checked ";
+        }
+        return '<div class="' + classes + '"><label for="'
+            + domId
+            + '">' + label + '</label>'
+            + '<input type="checkbox"' + checked + 'name="' + domId + '" id="' + domId + '" />'
+            + '</div>';
     }
 
-    return {
-        x: rotated.x + origin.x,
-        y: rotated.y + origin.y
+    constructor(parentId, classes, label, value, onChange) {
+        this.labelText = label + "&nbsp;";
+        this.value = value;
+        this.onChange = onChange;
+
+        var self = this;
+
+        this.domId = "ui-" + label + nDigitRand(3);
+        this.element = $(Checkbox.html(this.domId, this.value, this.labelText, classes));
+
+        var parentElement = $("#" + parentId);
+        parentElement.append(this.element);
+        parentElement.on("change", "#" + this.domId, function(e) {
+            var val = $(e.target).is(":checked");
+            self.onChange(val);
+        })
+
+        this.onChange(this.value);
     }
 }
 
-function inBounds(coords, boundingRect) {
-    var x = coords.x;
-    var y = coords.y;
-    return !(x < boundingRect.x || x > boundingRect.x + boundingRect.w
-        || y < boundingRect.y || y > boundingRect.y + boundingRect.h);
-}
-
-function sign (p1, p2, p3)
-{
-    return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
-}
-
-function inBoundsTriangle (pt, v1, v2, v3)
-{
-    var d1, d2, d3;
-    var has_neg, has_pos;
-
-    d1 = sign(pt, v1, v2);
-    d2 = sign(pt, v2, v3);
-    d3 = sign(pt, v3, v1);
-
-    has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
-    has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
-
-    //return true if all same sign (if on same side of all lines -> inside triangle)
-
-    return !(has_neg && has_pos);
-}
-
-
-function dist(c1, c2) {
-    var dx = c1.x - c2.x;
-    var dy = c1.y - c2.y;
-
-    return Math.sqrt(dx * dx + dy * dy);
-}
-
-function axiom4(line, pt) {
-    var x1 = line.c1.x;
-    var y1 = line.c1.y;
-    var x2 = line.c2.x;
-    var y2 = line.c2.y;
-    var x3 = pt.x;
-    var y3 = pt.y;
-
-    var m = (y2-y1) / (x2-x1);
-
-    var thresh = .001;
-
-    // Handle vert/horiz cases
-    if (Math.abs(x2-x1) < thresh) {
-        return {x : x2, y: y3};
-    } else if (Math.abs(y2-y1) < thresh) {
-        return {x: x3, y: y2};
+class NumberInput {
+    static html(domId, value, label, classes) {
+        return '<div class="' + classes + '"><label for="'
+            + domId
+            + '">' + label + '</label>'
+            + '<input type="number" value="' + value + '" name="' + domId + '" id="' + domId + '" />'
+            + '</div>';
     }
 
-    var x = ( -y1 + y3 + m*x1 + (x3/m) ) / (m + (1/m));
-    var y = y1 + m*x - m*x1;
+    constructor(parentId, classes, label, value, onChange) {
+        this.labelText = label + ":&nbsp;";
+        this.value = parseInt(value);
+        this.onChange = onChange;
 
-    return {x: x, y: y};
+        var self = this;
+
+        this.domId = "ui-" + label + nDigitRand(3);
+        this.element = $(NumberInput.html(this.domId, this.value, this.labelText, classes));
+
+        var parentElement = $("#" + parentId);
+        parentElement.append(this.element);
+        parentElement.on("change", "#" + this.domId, function(e) {
+            var val = parseInt($(e.target).val());
+            self.onChange(val);
+        })
+
+        this.onChange(value);
+
+    }
 }
+
+class FloatInput {
+    static html(domId, value, label, classes) {
+        return '<div class="' + classes + '"><label for="'
+            + domId
+            + '">' + label + '</label>'
+            + '<input type="text" value="' + value + '" name="' + domId + '" id="' + domId + '" />'
+            + ' * '
+            + '<select><option value=1 selected>1</option>'
+            + '<option value=2>&#8730; 2</option>'
+            + '<option value=3>&#8730; 3</option></select>'
+            + '</div>';
+    }
+
+    constructor(parentId, classes, label, value, onChange) {
+        this.labelText = label + ":&nbsp;";
+        this.value = parseFloat(value);
+        this.onChange = onChange;
+
+        var self = this;
+
+        this.domId = "ui-" + label + nDigitRand(3);
+        this.element = $(FloatInput.html(this.domId, this.value, this.labelText, classes));
+
+        var parentElement = $("#" + parentId);
+        parentElement.append(this.element);
+        parentElement.on("change", "#" + this.domId, function(e) {
+            var val = $(e.target).val();
+            self.onChange(val);
+        })
+
+        this.onChange(value);
+
+    }
+}
+
+var cb1 = new Checkbox("ui-settings", "base", "foo", true, function(value) {
+    console.log(value);
+});
+
+var fp1 = new FloatInput("ui-settings", "base", "foo1", 0.01, function(value) {
+    console.log(value);
+})
+
+var n1 = new NumberInput("ui-settings", "base", "bar", 500, function(value) {
+    console.log(value);
+})
